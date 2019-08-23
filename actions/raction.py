b6c6ad753d997afe30605fbe1110ce00cbf2d2e6
jhilmiljain <41698967+jhilmiljain@users.noreply.github.com>
2019-08-23 15:45:26 +0530

    Update raction.py

--- actions/raction.py
+++ actions/raction.py
@@ -12,6 +12,6 @@ class abc(Action):
 			print(response.status_code)
 			print(response.url)
 			
-		except requests.exception.MissingSchema:
+		except requests.exceptions.MissingSchema:
 			print("Wrong URL")
 			sys.exit(0)
